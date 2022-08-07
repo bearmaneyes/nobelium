85915f878b9042ebfd5d965600a64130bfdb64dd
bearmaneyes <75205214+bearmaneyes@users.noreply.github.com>
2022-08-07 21:45:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'Bearmaneyes',
+  author: 'Bearman',
+  email: 'N/V',
+  link: 'coming soon...',
+  description: 'Lif knowledge & Views.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
